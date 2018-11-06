a282e4b7c6a59efd087db3b55cc7c77e6df2a52e
Viktor Savonin <viktor.savonin@meawallet.com>
2018-11-06 16:59:17 +0200

    BaseEntity introduced, Optimistic locking with @Version, created_at and updated_at data fields

--- src/main/java/lv/javaguru/java3/core/domain/Client.java
+++ src/main/java/lv/javaguru/java3/core/domain/Client.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="clients")
-public class Client {
+public class Client extends BaseEntity {
 
     @Id
     @GeneratedValue(generator = "clients_seq", strategy = GenerationType.SEQUENCE)
@@ -41,6 +41,35 @@ public class Client {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client client = (Client) o;
+
+        if (id != null ? !id.equals(client.id) : client.id != null) return false;
+        if (login != null ? !login.equals(client.login) : client.login != null) return false;
+        return password != null ? password.equals(client.password) : client.password == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 
 }
